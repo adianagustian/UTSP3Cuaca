0d2e2dd3576e330557b9c68ddcdd8a84b8ca0d4d
Trisno Marsono <trisnomarsono@yahoo.com>
2017-07-31 19:57:07 +0700

    UTS Pemrograman 3

---
+++ src/utsp3cuaca/UTSP3Cuaca.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utsp3cuaca;
+
+/**
+ *
+ * @author adiaa
+ */
+public class UTSP3Cuaca {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FrmCuaca out = new FrmCuaca();
+        out.setVisible(true);
+    }
+    
+}
